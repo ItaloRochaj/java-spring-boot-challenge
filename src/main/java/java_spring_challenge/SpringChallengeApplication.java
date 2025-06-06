ab11916babc5aaa015c9a4aae7a8c93df176127d
Italo Rocha <italorochaj@icloud.com>
2025-06-06 12:03:50 -0300

    Testes

--- src/main/java/java_spring_challenge/SpringChallengeApplication.java
+++ src/main/java/java_spring_challenge/SpringChallengeApplication.java
@@ -2,12 +2,12 @@ package java_spring_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("java_spring_challenge.domain.model")  // ← Add this
 public class SpringChallengeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringChallengeApplication.class, args);
 	}
-
 }
--- src/main/java/java_spring_challenge/repository/PedidoRepository.java
+++
@@ -1,6 +0,0 @@
-package java_spring_challenge.repository;
-
-import java_spring_challenge.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {}
